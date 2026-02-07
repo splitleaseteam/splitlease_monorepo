621e1ea45c9ba1088a8d0b0958bc565bd0b9c3e0
splitleaseteam <splitleaseteam@gmail.com>
2026-02-07 09:30:06 -0600

    Delete pattern_3 directory

--- pattern_3/frontend/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * PATTERN 3: PRICE ANCHORING - HOOKS INDEX
- * Central export for all custom hooks
- */
-
-export * from './usePriceAnchor';
-export * from './useSavingsCalculations';
--- pattern_3/frontend/utils/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * PATTERN 3: PRICE ANCHORING - UTILITIES INDEX
- * Central export for all utility functions
- */
-
-export * from './priceAnchoring';
-export * from './formatting';
